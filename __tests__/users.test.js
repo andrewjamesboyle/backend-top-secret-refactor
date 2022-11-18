62bd66ad189a2b26c2f9c8ba4b8f04901787f09a
Andrew James Boyle <109110419+andrewjamesboyle@users.noreply.github.com>
2022-11-18 11:31:30 -0800

    create user test written

---
+++ __tests__/users.test.js
@@ -0,0 +1,44 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
+
+const mockUser = {
+  firstName: 'Test',
+  lastName: 'User',
+  email: 'test@example.com',
+  password: '12345',
+};
+
+// Register and Login Function for Testing
+const registerAndLogin = async (userProps = {}) => {
+  const password = userProps.password ?? mockUser.password;
+  const agent = request.agent(app);
+  const user = await UserService.create({ ...mockUser, ...userProps });
+  const { email } = user;
+  await agent.post('/api/v1/users/sessions').send({ email, password });
+  return [agent, user];
+};
+
+describe('top-secret users tests', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  it('creates a new user', async () => {
+    const res = await request(app).post('/api/v1/users').send(mockUser);
+    const { firstName, lastName, email } = mockUser;
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      firstName,
+      lastName,
+      email,
+    });
+
+    afterAll(() => {
+        pool.end();
+      });
+    });
+  });
