b858c10246a0af11ada279995b4863b1ebe21c4c
Andrew James Boyle <109110419+andrewjamesboyle@users.noreply.github.com>
2022-11-21 09:44:37 -0800

    secrets SQL setup

--- sql/setup.sql
+++ sql/setup.sql
@@ -9,3 +9,13 @@ CREATE TABLE users (
     email TEXT NOT NULL UNIQUE,
     password_hash TEXT NOT NULL
 );
+
+DROP TABLE IF EXISTS secrets;
+
+CREATE TABLE secrets (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    title TEXT NOT NULL,
+    description TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
